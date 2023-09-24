929c012566ebb408ed5c3b93abaf0b084c75db90
Muhammad Naufal <naufalmhd24@gmail.com>
2023-09-24 15:11:10 +0700

    fix todosModule import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { TodosModule } from './todos/todos.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(TodosModule);
   await app.listen(3000);
 }
 bootstrap();
