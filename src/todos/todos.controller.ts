d15af793011ddf4fd845f1bc5d948a710abcfbed
Muhammad Naufal <naufalmhd24@gmail.com>
2023-09-25 19:55:02 +0700

    implementing controller for service and repository

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,20 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTodoDto } from './dtos/create-todo.dto';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
+    constructor(private readonly todosService: TodosService) {}
+
     @Get()
     listTodos() {
-        return 'list todos';
+        return this.todosService.findAll();
     }
 
     @Post()
     createTodo(@Body() body: CreateTodoDto) {
-        return body;
+        return this.todosService.create(body.content);
     }
 
     @Get('/:id')
     getTodo(@Param('id') id: string) {
-        return `get todo ${id}`;
+        return this.todosService.findOne(+id);
     }
 }
