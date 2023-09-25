c02ffd1c9a6e2b59d5c49b38d9263c3e491d585a
Muhammad Naufal <naufalmhd24@gmail.com>
2023-09-25 19:24:39 +0700

    added a repository for todos module

---
+++ src/todos/todos.repository.ts
@@ -0,0 +1,29 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class TodosRepository {
+    async findAll() {
+        const data = await readFile('todos.json', 'utf-8');
+        return JSON.parse(data);
+    }
+
+    async findOne(id: number) {
+        const data = await readFile('todos.json', 'utf-8');
+        const todos = JSON.parse(data);
+
+        return todos.find((todo: any) => todo.id === id);
+    }
+
+    async create(todo: string) {
+        const data = await readFile('todos.json', 'utf-8');
+        const todos = JSON.parse(data);
+
+        const newTodo = {
+            id: todos[todos.length - 1].id + 1,
+            content: todo,
+        };
+
+        todos.push(newTodo);
+
+        await writeFile('todos.json', JSON.stringify(todos, null, 2), 'utf-8');
+    }
+}
